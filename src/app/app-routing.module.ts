34f22d353e8cd080b35a828a4123a851633767b6
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-04-10 09:49:32 -0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ErrorComponent } from './shared/error/error.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule) },
   { path: '404', component: ErrorComponent },
   { path: '**', redirectTo: '404' }
 ]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
-import { AppRoutingModule } from '../app-routing.module';
-
-
-
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
@@ -15,10 +13,13 @@ import { AppRoutingModule } from '../app-routing.module';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AuthRoutingModule
+
 
   ],
   exports: [
+
+
 
   ]
 })
