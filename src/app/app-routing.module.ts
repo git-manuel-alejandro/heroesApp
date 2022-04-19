21ee3aaf57100ba2e000f805c2c50e0f2ec374bb
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-04-19 12:44:51 -0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanLoad, CanActivate } from '@angular/router';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { ErrorComponent } from './shared/error/error.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule) },
+  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule), canLoad: [AuthGuard] },
   { path: '404', component: ErrorComponent },
   { path: '**', redirectTo: '404' }
 ]
