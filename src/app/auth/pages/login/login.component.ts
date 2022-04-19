9e4421a94606be295f3e96b7cca4a7726a289a78
manuel roa ojeda <manuel.roaojeda23@gmail.com>
2022-04-19 11:14:19 -0400

    commit

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
-  ngOnInit(): void {
+
+  login() {
+    this.authService.loginService().subscribe(res => {
+      if (res.id) {
+        this.router.navigate(['./heroes/listado'])
+      }
+    })
   }
 
 }
